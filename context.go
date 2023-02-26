1e773659262727d2b218a9c2b75ff503bb1deddc
Jacob Vallejo <jake@jahkeup.com>
2023-02-25 17:01:40 -0800

    chore: docstrings

--- context.go
+++ context.go
@@ -2,11 +2,14 @@ package testthings
 
 import "context"
 
+// C provides a context cleaned up with tests. A convenience function.
 func C(testingT Cleanuper) context.Context {
+	// callers wanting short and sweet don't get the cancel function :P
 	ctx, _ := NewContext(testingT)
 	return ctx
 }
 
+// NewContext creates a context that's cancelled when the testing.TB scope ends.
 func NewContext(testingT Cleanuper) (context.Context, context.CancelFunc) {
 	ctx, cancel := context.WithCancel(context.Background())
 	testingT.Cleanup(cancel)
