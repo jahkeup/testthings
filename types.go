d9aea36da8a270626d9d3fcd2fcf2522ea6352c2
Jacob Vallejo <jake@jahkeup.com>
2023-02-25 16:27:16 -0800

    chore: docstrings

--- types.go
+++ types.go
@@ -1,9 +1,14 @@
 package testthings
 
+// Cleanuper describe types that can cleanup after themselves and that allow
+// adding functions to be called when they're cleaning up. In practice, this is
+// really just a scoped-down testing.TB.
 type Cleanuper interface {
 	Cleanup(func())
 }
 
+// Logger describes types that can emit log messages. In practice, this is
+// really just a scoped-down testing.TB.
 type Logger interface {
 	Log(args ...any)
 }
